5580867e0ba69baafa0e532e4e14806106344737
Danilo Felipe <danilovex@gmail.com>
2016-07-14 09:09:39 -0300

    Testando catch do service getUser

--- test/services/user.test.js
+++ test/services/user.test.js
@@ -9,6 +9,7 @@ const sinon = require('sinon'),
 
 sinonStubPromise(sinon);
 
+
 var dao = {
   save: function(err, data) {},
   getUser: function(err, data) {}
@@ -20,9 +21,6 @@ var app = {
   }
 };
 
-var stubSave = sinon.stub(app.dao.user, 'save');
-var stubGetUser = sinon.stub(app.dao.user, 'getUser');
-
 var res = {
   status: function(x) {
     return res;
@@ -32,25 +30,25 @@ var res = {
   }
 };
 
-before(function() {
-  var mock = [
-    {
-      _id: '5786cf8373e4513352c1aaa3',
-      nome: 'João',
-      email: 'joao@gmail.com',
-      senha: 'UAFDF4343434343DDD',
-      telefones: [
-        {ddd: 11, numero: 33905221},
-        {ddd: 12, numero: 33905221}
-      ],
-      data_criacao: '2016-07-13T23:32:19.233Z',
-      data_atualizacao: '2016-07-13T23:32:19.233Z',
-      ultimo_login: '2016-07-13T23:32:19.233Z'
-    }
-  ];
-  stubSave.returnsPromise().resolves(mock);
-  stubGetUser.returnsPromise().resolves(mock);
-});
+var stubSave = sinon.stub(app.dao.user, 'save');
+var stubGetUser = sinon.stub(app.dao.user, 'getUser');
+
+var mockErro = 'Exception banco';
+var mockSuccess = [
+  {
+    _id: '5786cf8373e4513352c1aaa3',
+    nome: 'João',
+    email: 'joao@gmail.com',
+    senha: 'UAFDF4343434343DDD',
+    telefones: [
+      {ddd: 11, numero: 33905221},
+      {ddd: 12, numero: 33905221}
+    ],
+    data_criacao: '2016-07-13T23:32:19.233Z',
+    data_atualizacao: '2016-07-13T23:32:19.233Z',
+    ultimo_login: '2016-07-13T23:32:19.233Z'
+  }
+];
 
 describe('Service Usuário', function() {
 
@@ -60,6 +58,11 @@ describe('Service Usuário', function() {
       senha: '123456'
   };
 
+  before(function() {
+    stubSave.returnsPromise().resolves(mockSuccess);
+    stubGetUser.returnsPromise().resolves(mockSuccess);
+  });
+
   it('Deve retornar mensagem que email já existe', function() {
 
   service(app).save(user).then(function(data){
@@ -68,6 +71,21 @@ describe('Service Usuário', function() {
     stubSave.called.should.be.equal(false);
   });
 });
+
+before(function() {
+  stubGetUser.returnsPromise().resolves(null);
+  stubGetUser.returnsPromise().rejects(mockErro);
+});
+
+it('Deve exception ao validar se email existe', function() {
+
+service(app).save(user).catch(function(err){
+  assert.equal(err, mockErro);
+  stubGetUser.called.should.be.equal(true);
+  stubSave.called.should.be.equal(false);
+});
+});
+
 
 
 });
